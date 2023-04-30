a102cf42b91527183da82f50cf71488097b99840
Chris Messa <blindsun95@gmail.com>
2023-04-30 11:56:13 -0500

    initial project set up

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+    {
+        type: input,
+        message: '',
+        name: '',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(response => {
+        const readmeTemplate = ``
+        fs.writeFile(fileName, response, err => {
+            err? console.log(err) : console.log('File Written');
+        })
+    })
+}
 
 // Function call to initialize app
 init();
